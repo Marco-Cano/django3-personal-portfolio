cebf09e10007bd47e16aea6a6091dc1979aaa029
Marco <marco62412a@gmail.com>
2021-06-30 23:48:43 +0000

    Remove old stuff

--- Django_Portfolio/settings.py
+++ Django_Portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@1#w#!-)w$j9l4n^^9rfm#7mxe$%uezzdev&v^o6l_hnc6^vbu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cheesymarco.pythonanywhere.com']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
